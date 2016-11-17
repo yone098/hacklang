d49ab3506834e885db300c21d8db7383e02c2751
yone098 <yone098@gmail.com>
2016-11-17 09:10:50 +0000

    add void and noreturn sample.

--- samples/type_system/primitive/primitive_sample.php
+++ samples/type_system/primitive/primitive_sample.php
@@ -6,13 +6,15 @@ class A {}
 
 class B {
   public string $a;
-
   protected int $b;
+  protected bool $c = true;
+  public float $d = 1.23;
 
   public function __construct() {
     $this->a = "Hello";
     $this->b = 123;
   }
+
 }
 
 function bar(): bool {
@@ -24,5 +26,23 @@ function bar(): bool {
   return true;
 }
 
-
 bar();
+
+// void 
+function foo(): void {
+  echo "foo(): void\n";
+}
+
+foo();
+
+
+// noreturn
+function hoge(bool $b): noreturn {
+  if ($b) {
+    throw new \Exception("NoReturn No Life\n");
+  } else {
+    die("NoReturn No Life\n");
+  }
+}
+
+hoge(true);
