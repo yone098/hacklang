5cf87e7881b51fb57d0d8950d39bd862658bad47
yone098 <yone098@gmail.com>
2016-11-21 00:57:32 +0000

    modify generics samples.

--- samples/type_system/generics.php
+++ samples/type_system/generics.php
@@ -2,46 +2,99 @@
 
 namespace type_system;
 
-class Box<T> {
+// generic class
+class Animal<T> {
+  private ?T $name;
 
-  private array<T> $contents;
-
-  public function __construct() {
-    $this->contents = array();
+  public function set_name(?T $name): void {
+    $this->name = $name;
   }
 
-  public function put(T $x): void {
-    $this->contents[] = $x;
+  public function get_name(): ?T {
+    return $this->name;
   }
 
-  public function get(): array<T> {
-    return $this->contents;
+  public function get_age(): int {
+    return 1;
   }
 }
 
-function gift<T>(Box<T> $box, T $item): void {
-  $box->put($item);
+// generic method
+function generic_method<T>(Animal<T> $cat): ?T {
+  $value = $cat->get_name();
+
+  return $value;
 }
 
-function ts_generics_1(): array<arraykey> {
-  $box = new Box();
-  gift($box, "Hello");
-  gift($box, "Goodbye");
-  gift($box, 3);
-  return $box->get();
+// Constaraints
+class Wanko {
+  public function get_age(): int {
+    return 1;
+  }
 }
 
-function ts_generics_2(): array<arraykey> {
-  $box = new Box();
-  gift($box, "Hello");
-  gift($box, "Goodbye");
-  gift($box, 3);
-  return $box->get();
+class Dog extends Wanko {
+  public function get_age(): int {
+    return 2;
+  }
+
+  public function is_dog(): bool {
+    return true;
+  }
 }
+
+class Poodle<T> {
+  private T $data;
+
+  public function __construct(T $d) {
+    $this->data = $d;
+  }
+
+  public function get(): T {
+    return $this->data;
+  }
+}
+
+class TeacupPoodle<T as Wanko> extends Poodle<T> {
+  private int $sum = 0;
+
+  public function __construct(T $wanko) {
+    parent::__construct($wanko);
+    $this->sum += $wanko->get_age();
+  }
+}
+
 
 function run(): void {
-  var_dump(ts_generics_1());
-  var_dump(ts_generics_2());
+  // Animal クラスを生成
+  $dog = new Animal();
+  $dog->set_name("Pochi");
+  echo "dog->get_name():" . $dog->get_name() . "\n";
+
+  // Animal クラスを生成
+  $cat = new Animal();
+  $cat->set_name("Mike");
+
+  // generic_method の引数に Animal<string> を指定
+  $cat_name = generic_method($cat);
+  var_dump($cat_name);
+
+  // Dog クラスを生成
+  $dog = new Dog();
+  echo "=== dog ===\n";
+  var_dump($dog);
+
+  // TeacupPoodle クラスを生成
+  $teacup_poodle = new TeacupPoodle($dog);
+  echo "=== teacup_poodle ===\n";
+  var_dump($teacup_poodle);
+
+  // TeacupPoodle<Dog as Wanko> -> get() <Dog> がコールされる
+  $poodle2 = $teacup_poodle->get();
+  echo "=== poodle2 ===\n";
+  var_dump($poodle2);
+  echo "=== poodle2->is_dog() ===\n";
+  var_dump($poodle2->is_dog());
 }
 
 run();
