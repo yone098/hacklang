b2c479bb950f104c36f0e28e9ad479ae55ef4416
yone098 <yone098@gmail.com>
2016-12-28 00:10:02 +0000

    add refining nullable sample.

---
+++ samples/type_system/ref_nullable.php
@@ -0,0 +1,20 @@
+<?hh
+
+namespace type_system\refining\nullable;
+
+// Refining a type basically establishes that a value of one type is also of another type.
+function foo(?int $x): int {
+  $a = 4;
+  if ($x !== null) { // refine $x to just an int by verifying it is not null
+    return $x + $a; // guaranteed that $x is not null now
+  }
+  return $a;
+}
+
+function run(): void {
+  echo "===== start types:refining nullable sample =====" . PHP_EOL;
+  var_dump(foo(5));
+  var_dump(foo(null));
+}
+
+\type_system\refining\nullable\run();
