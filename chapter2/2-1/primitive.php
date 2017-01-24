32b2e0a743f5a56559be6d2af9ad18515d6bbd18
yone098 <yone098@gmail.com>
2017-01-24 08:33:11 +0000

    add 2-1 primitive.php

---
+++ chapter2/2-1/primitive.php
@@ -0,0 +1,47 @@
+<?hh
+
+namespace type_system\primitive;
+
+class A {}
+
+class B {
+  public string $a;
+  protected int $b;
+  protected bool $c = true;
+  public float $d = 1.23;
+
+  public function __construct() {
+    $this->a = "Hello";
+    $this->b = 123;
+  }
+
+}
+
+function bar(): bool {
+
+  $b = new \type_system\primitive\B();
+
+  var_dump($b);
+
+  return true;
+}
+
+bar();
+
+// void 
+function foo(): void {
+  echo "foo(): void\n";
+}
+
+foo();
+
+// noreturn
+function main(bool $b): noreturn {
+  if ($b) {
+    throw new \Exception("NoReturn No Life\n");
+  } else {
+    die("NoReturn No Life\n");
+  }
+}
+
+main(true);
