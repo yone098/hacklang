f81802254392f73867f08478d138d3f02ed71715
yone098 <yone098@gmail.com>
2017-02-02 12:11:08 +0000

    add shapes samples.

---
+++ chapter3/3-2/shapes.php
@@ -0,0 +1,18 @@
+<?hh
+
+namespace type_system\shapes;
+
+type customer = shape('id' => int, 'name' => string);
+
+function create_user(int $id, string $name): customer {
+  return shape('id' => $id, 'name' => $name);
+}
+
+function run(): void {
+  echo "===== start shapes sample =====" . PHP_EOL;
+  $c = create_user(0, "James");
+  var_dump($c['id']);
+  var_dump($c['name']);
+}
+
+\type_system\shapes\run();
