7ed36a7b9a6cb9b609e4b0991c651400d14f4e0c
yone098 <yone098@gmail.com>
2017-02-07 06:34:17 +0000

    update collection sample.

---
+++ app.php
@@ -0,0 +1,9 @@
+<?hh
+
+namespace chapter3\collections;
+
+function run(): void {
+  echo "===== start collection sample =====" . PHP_EOL;
+}
+
+\chapter3\collections\run();
--- chapter3/3-1/collection.php
+++ chapter3/3-1/collection.php
@@ -2,8 +2,21 @@
 
 namespace chapter3\collections;
 
+function use_vector(): void {
+  echo "===== Vector sample =====" . PHP_EOL;
+  $fruits = Vector{ 'Apple', 'Orange' };
+  $fruits[] = 'Grape';
+  foreach ($fruits as $fruit) {
+    echo "fluits: " . $fruit . PHP_EOL;
+  }
+  // 値を変更
+  $fruits[1] = 'Banana';
+  var_dump($fruits);
+}
+
 function run(): void {
   echo "===== start collection sample =====" . PHP_EOL;
+  use_vector();
 }
 
 \chapter3\collections\run();
