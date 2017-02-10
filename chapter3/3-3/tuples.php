37d689963ec620f9acc23f171546f827c0b8cb81
yone098 <yone098@gmail.com>
2017-02-10 09:51:56 +0000

    update tuples samples.

--- chapter3/3-3/tuples.php
+++ chapter3/3-3/tuples.php
@@ -7,10 +7,15 @@ class A {
   public static $t2 = tuple(array(1, 2), Vector {3, 4});
 }
 
+function any_returns(): (int, string) {
+  return tuple(123, "yone098");
+}
+
 function run(): void {
   echo "===== start tuples sample ======" . PHP_EOL;
   var_dump(new A());
   var_dump(A::$t2);
+  var_dump(any_returns());
 }
 
 
