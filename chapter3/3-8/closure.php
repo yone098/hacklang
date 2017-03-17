5b63a642b21ef330b1c9b8cc48291bb5b669350e
yone098 <yone098@gmail.com>
2017-03-17 07:09:28 +0000

    add closure sample.

---
+++ chapter3/3-8/closure.php
@@ -0,0 +1,26 @@
+<?hh
+
+namespace callables\closure;
+
+function my_math((function(int, bool): int) $callback, int $a, bool $b): ?int {
+  if ($a < 0) {
+    return null;
+  }
+
+  return $callback($a, $b);
+}
+
+function run(): void {
+  $callback = function(int $a, bool $b): int {
+    if ($b) {
+      return $a + $a;
+    }
+    return $a * $a;
+  };
+
+  var_dump(my_math($callback, 3, true));
+  var_dump(my_math($callback, 3, false));
+  var_dump(my_math($callback, -1, false));
+}
+
+\callables\closure\run();
